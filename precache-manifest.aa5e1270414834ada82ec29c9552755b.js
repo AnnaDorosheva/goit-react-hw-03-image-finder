bd90932682965a3d06e1860cb0a0904bd61f904e
AnnaDorosheva <annador@ukr.net>
2019-12-24 16:16:52 +0200

    Updates

---
+++ precache-manifest.aa5e1270414834ada82ec29c9552755b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9076485dd1b11b6391b8e52395328c44",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c2bfbe7e312071256221",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7df73250.chunk.css"
+  },
+  {
+    "revision": "e9b290737ad9cf671c41",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.4a8340e3.chunk.js"
+  },
+  {
+    "revision": "c2bfbe7e312071256221",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.7be40302.chunk.js"
+  },
+  {
+    "revision": "73679450c42f7cb5c5a9",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.6fc5ae67.js"
+  }
+]);
